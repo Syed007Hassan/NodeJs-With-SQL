2cb162d8bd29d94db674c0fcc5ff335f8597ec98
Syed007Hassan <k201052@nu.edu.pk>
2022-10-04 13:02:02 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// dependencies
+const express = require("express");
+const app = express();
+
+const mysql = require('mysql2');
+
+app.use(express.static('public'));
+
+const db = mysql.createConnection({
+  host: 'localhost:3306',
+  user: 'root',
+  password: 'Hppavilion15',
+  database: 'TestDb'
+});
+
+
+
+app.get("/", (req, res) => {
+
+   const fName = "Hassan";
+   const lName = "ali";
+
+   
+   db.query(
+    'INSERT INTO users (fName,lName) VALUES ("Hassan","Ali")',
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+
+
+
+});
+
+
+
+// PORT
+const PORT = 3000;
+
+app.listen(PORT, () => {
+   console.log(`Server is running on PORT: ${PORT}`);
+});
